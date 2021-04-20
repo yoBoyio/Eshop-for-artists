ac9436e89cbcdab22421e2fa30dc84569501ac9a
yoBoyio <dimi044@hotmail.com>
2021-04-20 03:02:32 +0300

    Add firebase oauth and searchbox

--- frontend/src/redux/reducers/dataReducers.js
+++ frontend/src/redux/reducers/dataReducers.js
@@ -1,27 +1,35 @@
 import {
-    LOADING_DATA,
-    SET_HISTORY,
-  } from '../type';
+  LOADING_DATA,
+  SET_HISTORY,
+  GET_SEARCHDATA
+} from '../type';
 
 const initialState = {
-    history: [],
-    loading: false
-  };
+  history: [],
+  search: [],
+  loading: false
+};
 
-  export default function(state = initialState, action) {
-    switch (action.type) {
-      case LOADING_DATA:
-        return {
-          ...state,
-          loading: true
-        };
-      case SET_HISTORY:
-        return {
-          ...state,
-          history: action.payload,
-          loading: false
-        }; 
-      default:
-        return state;
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case LOADING_DATA:
+      return {
+        ...state,
+        loading: true
+      };
+    case SET_HISTORY:
+      return {
+        ...state,
+        history: action.payload,
+        loading: false
+      };
+    case GET_SEARCHDATA:
+      return {
+        ...state,
+        search: action.payload,
+        loading: false
+      };
+    default:
+      return state;
   }
 }
